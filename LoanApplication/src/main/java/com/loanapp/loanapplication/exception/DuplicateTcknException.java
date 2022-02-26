526ac0600f49e1c8c44ab638ead08a6e8026e6a1
Emin <58512029+eminyilmazz@users.noreply.github.com>
2022-02-27 00:49:34 +0300

    Delete LoanApplication directory
    Accidental push

--- LoanApplication/src/main/java/com/loanapp/loanapplication/exception/DuplicateTcknException.java
+++
@@ -1,9 +0,0 @@
-package com.loanapp.loanapplication.exception;
-
-public class DuplicateTcknException extends RuntimeException{
-
-    private static final String genericMessage = "Provided TCKN already exists.\nCannot accept duplicate TCKN.\n";
-    public DuplicateTcknException() {
-        super(genericMessage);
-    }
-}
--- LoanApplication/src/main/java/com/loanapp/loanapplication/exception/IllegalTcknException.java
+++
@@ -1,7 +0,0 @@
-package com.loanapp.loanapplication.exception;
-
-public class IllegalTcknException extends IllegalArgumentException {
-    public IllegalTcknException(String s) {
-        super(s);
-    }
-}
